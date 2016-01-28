db50e02e74b9e8aed2512d04e01ab171431429d9
Mandeep <mandeep.singh.bhatia@gmail.com>
2016-01-28 18:30:48 +0800

    use chrono from c++11

--- opencog/spacetime/SpaceTimeAtomIndex.cc
+++ opencog/spacetime/SpaceTimeAtomIndex.cc
@@ -1,17 +1,20 @@
 #include "SpaceTimeAtomIndex.h"
 template <class handle>
-bool time_circle::put(handle atom,time_t t,location l)
+bool time_circle::put(handle atom,time_pt t,location l)
 {
-  int tt=get_time_unit(t);
-  struct tm tms;
+  int tt=get_time_unit(t);//negative distance from start if full rotation
+  //struct time_pt tms;
+  chrono::system_clock::duration dd;//seconds
 
   if (tt<0) {//fold all
     for (int a=0;a<vt.size();a++){
-      vt[a].m.clear;
+      vt[a].m.clear();
       //set start and current to same
     }
-    tms.tm_seconds=(-tt)*time_resolution
-    vt[0].t=start_t+mktime(tms); //check if this is correct
+    //tms.tm_seconds=(-tt)*time_resolution
+    dd=chrono::seconds((-tt)*time_resolution);
+    vt[0].t=start_t+dd; //check if this is correct
+    start_t=start_t+dd;
     start=0;
     stop=0;
     curr_t=0;
@@ -23,11 +26,12 @@ bool time_circle::put(handle atom,time_t t,location l)
     int dtt=time_unit_dist(start,tt);
     if (dtt>dt){
       for (int a=dt;a<dtt;a++){
-        vt[curr_t].t=start_t+a*time_resolution; //fix this for time calc
+        dd=chrono::seconds(a*time_resolution)
+        vt[curr_t].t=start_t+dd; //fix this for time calc
         vt[curr_t].m.clear();
-        move_to_next();
+        move_to_next();//modify curr_t
       }
-      vt[curr_t].t=start_t+dtt*time_resolution; // fix this
+      vt[curr_t].t=start_t+chrono::seconds(dtt*time_resolution); // fix this
       vt[curr_t].m[l]=atom;
     }
     else{
@@ -49,7 +53,7 @@ bool time_circle::get_atom(handle &h,time_t t,location l)
 template <class handle>
 void time_circle::get_times(handle atom,time_t_list& t,location l)
 {
-  //threaded search
+  //threaded search - boost threads
 }
 
 template <class handle>
--- opencog/spacetime/SpaceTimeAtomIndex.h
+++ opencog/spacetime/SpaceTimeAtomIndex.h
@@ -4,7 +4,8 @@
 #include <vector>
 #include <list>
 #include <map>
-#include <ctime>
+//#include <ctime>
+#include <chrono>
 using namespace std;
 
 //template <class handle>
@@ -21,13 +22,13 @@ struct location{
     return false;
   }
 };
-
+typedef chrono::system_clock::time_point time_pt;
 typedef list<location> location_list;
-typedef list<time_t> time_t_list;
+typedef list<time_pt> tm_list;
 //handle has to be pointer
 template <class handle>
 struct time_unit{
-  time_t t;
+  time_pt t;
   map<location,handle> m;
 };
 //cicular buffer
@@ -38,12 +39,13 @@ private:
   float originX,originY,originZ;
 
   int curr_t,start,stop;//start,stop is vector index of start,stop curr_t is index current vector of time
-  time_t start_t;//current start of time
+  time_pt start_t;//current start of time
   vector<time_unit<handle>>vt;
 
 public:
-  time_circle(unsigned int time_units,time_t start_time,float time_res_seconds,float space_resolution,float oX,float oY,float oZ):
+  time_circle(unsigned int time_units,time_pt start_time,float time_res_seconds,float space_resolution,float oX,float oY,float oZ):
   num_time_unit(time_units),curr_t(0),start(0),start_t(start_time),time_res(time_res_seconds){
+    //if time units is less than two then throw exception
     vt.reserve(time_units);
     /*
     for (int a=0;a<time_units;a++){
@@ -57,20 +59,23 @@ public:
     }
     */
   }
-  time_t get_start_time();
-  time_t get_stop_time();
-  int get_time_unit(time_t);//-neg if out of circle
+  time_pt get_start_time();
+  time_pt get_stop_time();
+  int get_time_unit(time_pt tp);//-neg if out of circle
 
   void get_res_origin(float& res,float& oX,float& oY,float& oZ){
     res=space_res;oX=originX;oY=originY;oZ=originZ;
   }
   inline unsigned int get_num_time_units(){return num_time_unit;}
   //API
+  int get_current_time_handle();
+  int next_new_time_unit();
   void set_start_time(time_t t);
-  bool put(handle atom,time_t t,location l);
-  bool get_atom(handle &h,time_t t,location l);
-  void get_times(handle atom,time_t_list& t,location l);
-  void get_locations(handle atom,time_t t,location_list& ll);
+  bool put(handle atom,int time_handle,location l)
+  bool put(handle atom,time_pt t,location l);
+  bool get_atom(handle &h,time_pt t,location l);
+  void get_times(handle atom,time_pt_list& t,location l);
+  void get_locations(handle atom,time_pt t,location_list& ll);
   //bool save(std::string file);//resolve atom uuid
   //bool load(std::string file);//
 };
