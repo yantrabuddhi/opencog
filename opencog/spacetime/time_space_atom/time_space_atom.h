1579ccd9e0d68e58137a6dc34ad95e66469efe44
Mandeep <mandeep.singh.bhatia@gmail.com>
2016-02-02 17:27:29 +0800

    editing api

--- opencog/spacetime/time_space_atom/time_space_atom.h
+++ opencog/spacetime/time_space_atom/time_space_atom.h
@@ -6,10 +6,29 @@
 
 #ifndef TIME_SPACE_ATOM_H
 #define TIME_SPACE_ATOM_H
+#include <iostream>
 #include <boost/circular_buffer.hpp>
 #include <list>
 #include <chrono>
 #include "TemplateOcTree.h"
 #include "TemplateOcTreeNode.h"
 
+typedef int Hanadle;
+class TimeSpaceAtom{
+	public:
+	//API
+	int AddMap(std::string name);
+	int GetMapHandle(std::string name);
+	bool SetCurrentTimeRange();
+	getCurrentTimeRange();
+	CreateNewTimeUnit();
+	int PutAtomCurrentTime(point3d location,const Handle& ato);
+	Handle GetAtomCurrentTime();
+	Handle GetAtomAtTime();
+	TimeList GetTimesOfAtomOccurenceAtLocation();
+	TimeList GetTimesOfAtomOccurence();
+	Timelist GetTimesOfAtomOccurenceInLocationBox();
+	point3d_list GetLocationsOfAtomOccurence();
+	point3d_list GetLocationsOfAtomOccurenceAtTime();
+};
 #end
