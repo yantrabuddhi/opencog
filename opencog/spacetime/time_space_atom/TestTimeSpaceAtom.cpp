2ea8896b6eebb6ab15a006b27dacf17e452029eb
Mandeep <mandeep.singh.bhatia@gmail.com>
2016-02-03 20:26:18 +0800

    fixed and started working test case

--- opencog/spacetime/time_space_atom/TestTimeSpaceAtom.cpp
+++ opencog/spacetime/time_space_atom/TestTimeSpaceAtom.cpp
@@ -1,4 +1,5 @@
 //TestTemplateOcTree.cpp
+#include "assert.h"
 #include "TimeSpaceAtom.h"
 //#include "TemplateOcTreeNode.h"
 
@@ -23,7 +24,8 @@ int main(int argc, char** argv) {
   cout << endl;
   cout << "generating example map" << endl;
 
-  vector<double> res(0.2,0.3,0.4,0.5);
+  double hi[4]={0.2,0.3,0.4,0.5};
+  vector<double> res(hi,hi+4);
   TimeSpaceAtom tsa(3,res);  // create empty tree with resolution 0.1
   time_pt t1=std::chrono::system_clock::now();
   duration_c dd=std::chrono::seconds(10);
@@ -36,14 +38,15 @@ int main(int argc, char** argv) {
     for (int y=-20; y<20; y++) {
       for (int z=-20; z<20; z++) {
         point3d endpoint ((float) x*0.05f, (float) y*0.05f, (float) z*0.05f);
-        tree.updateNode(endpoint, true); // integrate 'occupied' measurement
-        tree.setNodeData(endpoint, 21);//if omitted prune value of 0 is assigned?
+        assert(tsa.PutAtomAtCurrentTime(1,endpoint,21));
+        //tree.updateNode(endpoint, true); // integrate 'occupied' measurement
+        //tree.setNodeData(endpoint, 21);//if omitted prune value of 0 is assigned?
       }
     }
   }
 
   // insert some measurements of free cells
-
+/* Need to put something to delete atom
   for (int x=-30; x<30; x++) {
     for (int y=-30; y<30; y++) {
       for (int z=-30; z<30; z++) {
@@ -67,7 +70,7 @@ int main(int argc, char** argv) {
   query = point3d(1.,1.,1.);
   result = tree.search (query);
   print_query_info(query, result);
-
+*/
 
   cout << endl;
   /*
